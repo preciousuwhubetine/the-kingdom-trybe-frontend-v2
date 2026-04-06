ff67345c010efb22270e9a7a5d6e30f8f91386bd
Precious Uwhubetine <preciousbetine@gmail.com>
2026-04-06 09:10:34 +0100

    Replace broken svg icons

--- src/application/views/HomePage/HomePage.jsx
+++ src/application/views/HomePage/HomePage.jsx
@@ -1604,13 +1604,8 @@ function HomePage() {
             <Link to="/">
               Shop FaithDrop
 
-              <svg width="20" height="12" viewBox="0 0 20 12" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                  <marker id="a" viewBox="0 0 10 10" refX="8" refY="5" markerWidth="6" markerHeight="6" orient="auto">
-                    <path d="M2 1L8 5L2 9" fill="none" stroke="white" strokeWidth="1.5" strokeLinecap="round"/>
-                  </marker>
-                </defs>
-                <line x1="2" y1="6" x2="16" y2="6" stroke="white" strokeWidth="2" markerEnd="url(#a)"/>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 12H19M19 12L13 6M19 12L13 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
               </svg>
             </Link>
           </div>
